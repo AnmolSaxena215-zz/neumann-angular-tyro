1dfc779522f4deb31fd04102a1d25f6785277181
anuhitha-infrrd <anuhithan@infrrd.ai>
2020-11-18 11:19:18 +0530

    design changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,19 @@ import {HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoreComponent } from './core/core.component';
 import { AuthComponent } from './auth/auth.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './auth/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoreComponent,
     AuthComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
